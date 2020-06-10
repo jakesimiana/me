1332474c7de6a87caeceee4396b76854377aa762
Jake Simiana <jakesimiana@gmail.com>
2020-06-10 11:34:06 +1000

    11:30am

--- week2/exercise3.py
+++ week2/exercise3.py
@@ -7,7 +7,11 @@ def is_odd(a_number):
 
     Look into modulo division using the '%' operator as one way of doing this.
     """
-    return None
+if a_number % 2 = 0:
+    return False
+      
+else:
+    return True
 
 
 def fix_it(moves=True, should_move=True):
